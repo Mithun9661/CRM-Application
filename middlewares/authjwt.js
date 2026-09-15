ff15c2cc9d5e4d86296513a216b84a92aface793
Mithun Kumar <153515460+Mithun9661@users.noreply.github.com>
2026-09-15 11:05:26 +0530

    Allow platform and company admins in user management

--- middlewares/authjwt.js
+++ middlewares/authjwt.js
@@ -3,99 +3,34 @@ const config = require("../configs/auth.config");
 const User = require("../Models/user.model");
 const constants = require("../utils/constants");
 
-// ================= VERIFY JWT TOKEN =================
 const verifyToken = (req, res, next) => {
   let token = req.headers["x-access-token"];
-
-  // Swagger / Bearer Token support
-  if (!token && req.headers.authorization) {
-    const authHeader = req.headers.authorization;
-
-    if (authHeader.startsWith("Bearer ")) {
-      token = authHeader.split(" ")[1];
-    }
+  if (!token && req.headers.authorization?.startsWith("Bearer ")) {
+    token = req.headers.authorization.split(" ")[1];
   }
+  if (!token) return res.status(403).send({ message: "No Access token Passed!" });
 
-  if (!token) {
-    return res.status(403).send({
-      message: "No Access token Passed!",
-    });
-  }
-
-  // Verification of JWT Token
   jwt.verify(token, config.secret, (err, decoded) => {
-    if (err) {
-      return res.status(401).send({
-        message: "Unauthorized! Invalid token!",
-      });
-    }
-
+    if (err) return res.status(401).send({ message: "Unauthorized! Invalid token!" });
     req.userId = decoded.id;
     next();
   });
 };
 
-// ================= ADMIN ROLE CHECK =================
-const isAdmin = async (req, res, next) => {
+const roleGuard = (...roles) => async (req, res, next) => {
   try {
-    // Find user based on userId set by verifyToken middleware
-    const user = await User.findOne({
-      userId: req.userId,
-    });
-
-    if (!user) {
-      return res.status(404).send({
-        message: "User not found",
-      });
-    }
-
-    if (user.userType === constants.userType.admin) {
-      return next();
-    }
-
-    return res.status(403).send({
-      message: "Only Admin role is allowed to access this API!",
-    });
+    const user = await User.findOne({ userId: req.userId });
+    if (!user) return res.status(404).send({ message: "User not found" });
+    if (!roles.includes(user.userType)) return res.status(403).send({ message: "You are not authorized to access this API" });
+    req.currentUser = user;
+    next();
   } catch (err) {
-    console.error("Admin authorization error:", err.message);
-
-    return res.status(500).send({
-      message: "Internal server error during authorization",
-    });
+    return res.status(500).send({ message: "Internal server error during authorization" });
   }
 };
 
-// ================= SUPER ADMIN ROLE CHECK =================
-const isSuperAdmin = async (req, res, next) => {
-  try {
-    const user = await User.findOne({
-      userId: req.userId,
-    });
+const isAdmin = roleGuard(constants.userType.admin);
+const isAdminOrSuperAdmin = roleGuard(constants.userType.admin, constants.userType.superAdmin);
+const isSuperAdmin = roleGuard(constants.userType.superAdmin);
 
-    if (!user) {
-      return res.status(404).send({
-        message: "User not found",
-      });
-    }
-
-    if (user.userType === constants.userType.superAdmin) {
-      return next();
-    }
-
-    return res.status(403).send({
-      message: "Only Super Admin role is allowed to access this API!",
-    });
-  } catch (err) {
-    console.error("Super Admin authorization error:", err.message);
-
-    return res.status(500).send({
-      message: "Internal server error during authorization",
-    });
-  }
-};
-
-module.exports = {
-  verifyToken,
-  isAdmin,
-  isSuperAdmin,
-};
+module.exports = { verifyToken, isAdmin, isAdminOrSuperAdmin, isSuperAdmin };
