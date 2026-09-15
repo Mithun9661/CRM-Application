df11082777ee71ec65bc1bdf28ca483f5d481f55
Mithun Kumar <153515460+Mithun9661@users.noreply.github.com>
2026-09-15 11:05:37 +0530

    Enforce tenant isolation in user management

--- Controllers/user.controller.js
+++ Controllers/user.controller.js
@@ -1,77 +1,65 @@
-/**
- * This is controller files for suppoerting users related API object
-*/
-const objectConverter = require("../utils/objectConverter")
+const objectConverter = require("../utils/objectConverter");
+const User = require("../Models/user.model");
+const constants = require("../utils/constants");
 
+const sameCompany = (a, b) => String(a || "") === String(b || "");
 
-
-
-const User =require("../Models/user.model")
-
-/**
- * Controller to fetch all the user details.
- */
-exports.findAll = async(req,res)=>{
-
-    //Start supporting the query param
-    let userTypeReq = req.query.userType;
-    let userStatusReq = req.query.userStatus;
-    // console.log(userTypeReq);
-    // console.log(userStatus);
+exports.findAll = async (req, res) => {
+  try {
+    const currentUser = req.currentUser || await User.findOne({ userId: req.userId });
+    if (!currentUser) return res.status(401).send({ message: "User not found" });
 
     const queryObj = {};
-    if (userTypeReq){
-        queryObj.userType = userTypeReq;
+    if (currentUser.userType !== constants.userType.superAdmin) queryObj.companyId = currentUser.companyId;
+    if (req.query.userType) queryObj.userType = req.query.userType;
+    if (req.query.userStatus) queryObj.userStatus = req.query.userStatus;
 
-    }
-    if (userStatusReq){
-        queryObj.userStatus = userStatusReq;
-    }
-
-    console.log(queryObj);
-
-
-    const users = await User.find(queryObj);
-    console.log(users);
-    console.log(objectConverter.userResponse(users));
+    const users = await User.find(queryObj).sort({ createdAt: -1 });
     return res.status(200).send(objectConverter.userResponse(users));
+  } catch (err) {
+    return res.status(500).send({ message: "Error while fetching users" });
+  }
+};
 
-}
+exports.findById = async (req, res) => {
+  try {
+    const currentUser = req.currentUser || await User.findOne({ userId: req.userId });
+    const user = await User.findOne({ userId: req.params.userId });
+    if (!user) return res.status(404).send({ message: "User with the given id not present" });
 
-/**
- * Controller to find the user based on the user id
- */
-exports.findById = async(req , res)=>{
-    const userId = req.params.userId;
-    const user = await User.find({
-        userId : userId
-    });
-    if (user && user.length > 0){
-        return res.status(200).send(objectConverter.userResponse(user));
-    } else{
-        return res.status(400).send({
-            message : "User with the given id not present"
-        })
+    if (currentUser.userType !== constants.userType.superAdmin && !sameCompany(currentUser.companyId, user.companyId)) {
+      return res.status(403).send({ message: "You cannot access users from another company" });
     }
-}
+    return res.status(200).send(objectConverter.userResponse([user]));
+  } catch (err) {
+    return res.status(500).send({ message: "Error while fetching user" });
+  }
+};
 
-/**
- * Controller to update the user and it's details
- */
-exports.updateUser = async(req, res)=>{
-    const userIdReq = req.params.userId;
-    try{
-        const user = await User.findOneAndUpdate({userId : userIdReq},{
-           name : req.body.name,
-            userStatus : req.body.userStatus,
-            userType : req.body.userType
-        }).exec();
-        res.status(200).send({
-            message : 'user record has been successfully update'
-        })
-    }catch(err){
-        res.status(500).send({
-            message : "Some internal error while updating the user record"
-        })
+exports.updateUser = async (req, res) => {
+  try {
+    const currentUser = req.currentUser || await User.findOne({ userId: req.userId });
+    const target = await User.findOne({ userId: req.params.userId });
+    if (!target) return res.status(404).send({ message: "User not found" });
+
+    if (currentUser.userType !== constants.userType.superAdmin && !sameCompany(currentUser.companyId, target.companyId)) {
+      return res.status(403).send({ message: "You cannot update users from another company" });
     }
-}
+    if (currentUser.userType !== constants.userType.superAdmin && req.body.userType === constants.userType.superAdmin) {
+      return res.status(403).send({ message: "Company admin cannot create or promote a Super Admin" });
+    }
+
+    if (req.body.name !== undefined) target.name = req.body.name;
+    if (req.body.userStatus !== undefined) target.userStatus = req.body.userStatus;
+    if (req.body.userType !== undefined) target.userType = req.body.userType;
+
+    if (currentUser.userType === constants.userType.superAdmin && req.body.companyId !== undefined) {
+      target.companyId = req.body.companyId || null;
+    }
+
+    await target.save();
+    return res.status(200).send({ message: "User record has been successfully updated" });
+  } catch (err) {
+    return res.status(500).send({ message: err.message || "Some internal error while updating the user record" });
+  }
+};
