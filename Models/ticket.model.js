99b64deea98af9dd57376d30e29960b2d46bc62d
Mithun Kumar <153515460+Mithun9661@users.noreply.github.com>
2026-09-15 11:03:07 +0530

    Add tenant company isolation to tickets

--- Models/ticket.model.js
+++ Models/ticket.model.js
@@ -1,47 +1,26 @@
 const mongoose = require("mongoose");
 const constants = require("../utils/constants");
 
-
 const ticketSchema = new mongoose.Schema({
-    title : {
-        type : String,
-        required : true
+    title: { type: String, required: true, trim: true },
+    ticketPriority: { type: Number, required: true, default: 4 },
+    description: { type: String, required: true, trim: true },
+    status: { type: String, required: true, default: constants.ticketStatuses.open },
+    reporter: { type: String, required: true },
+    assignee: { type: String },
+    companyId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Company",
+        default: null,
+        index: true
     },
-    ticketPriority : {
-        type : Number,
-        required : true,
-        default : 4
-    },
-    description : {
-        type : String,
-        required : true
-    },
-    status : {
-        type : String,
-        required : true,
-        default : constants.ticketStatuses.open
-    },
-    reporter : {  //We will be used the userId
-        type : String,
-        required : true
-    },
-    assignee : {
-        type : String,
-    },
-    ticketHistory: [
-        {
-            action: String,
-            updatedBy: String,
-            oldValue: Object,
-            newValue: Object,
-            timestamp: {
-                type: Date,
-                default: Date.now
-            }
-        }
-    ]
+    ticketHistory: [{
+        action: String,
+        updatedBy: String,
+        oldValue: Object,
+        newValue: Object,
+        timestamp: { type: Date, default: Date.now }
+    }]
+}, { timestamps: true });
 
-
-},{timestamps : true});
-
-module.exports = mongoose.model("Ticket" , ticketSchema);
+module.exports = mongoose.model("Ticket", ticketSchema);
