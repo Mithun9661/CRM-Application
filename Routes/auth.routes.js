23a17bbb70e82cef808f1aa6083a4a3e9252095c
Mithun Kumar <153515460+Mithun9661@users.noreply.github.com>
2026-09-15 11:08:37 +0530

    Remove unsafe duplicate signup route

--- Routes/auth.routes.js
+++ Routes/auth.routes.js
@@ -1,17 +1,10 @@
-
-
 const express = require("express");
-
-
 const router = express.Router();
-
 const authController = require("../Controllers/auth.controller");
 const verifyUserReqBody = require("../middlewares/verifyUserReqBody");
 
-router.post("/auth/signup",[verifyUserReqBody.validateUserReqBody],authController.signup);
-router.post("/auth/signin",authController.signin);
-router.get("/", (req, res) => {
-    res.send("Auth Route Working");
-});
-router.post("/signup", authController.signup);
+router.post("/auth/signup", [verifyUserReqBody.validateUserReqBody], authController.signup);
+router.post("/auth/signin", authController.signin);
+router.get("/", (req, res) => res.send("Auth Route Working"));
+
 module.exports = router;
